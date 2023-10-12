70dc5d606ee69b3664364a1361137e7d7d22141b
Taha Daneshmand <spy404.work@gmail.com>
2023-10-12 17:05:55 +0000

    Problem 052

---
+++ 053.py
@@ -0,0 +1,17 @@
+from math import factorial
+
+
+def combinations(n, r):
+    return factorial(n) / (factorial(r) * factorial(n - r))
+
+
+def solution():
+    total = 0
+
+    for i in range(1, 101):
+        for j in range(1, i + 1):
+            if combinations(i, j) > 1e6:
+                total += 1
+    return total
+
+print(solution())
