7709f121309381e7ae526a05118ec9cac5eb252b
Taha Daneshmand <spy404.work@gmail.com>
2023-10-12 15:32:51 +0000

    Problem 0

---
+++ 030.py
@@ -0,0 +1,15 @@
+DIGITS_FIFTH_POWER = {str(digit): digit**5 for digit in range(10)}
+
+
+def digits_fifth_powers_sum(number: int) -> int:
+    return sum(DIGITS_FIFTH_POWER[digit] for digit in str(number))
+
+
+def solution() -> int:
+    return sum(
+        number
+        for number in range(1000, 1000000)
+        if number == digits_fifth_powers_sum(number)
+    )
+
+print(solution())
