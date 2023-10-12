4f3fa2d95025ed2ad80ba0b9e32c4991adda832b
Taha Daneshmand <spy404.work@gmail.com>
2023-10-12 17:05:00 +0000

    Problem 052

---
+++ 052.py
@@ -0,0 +1,17 @@
+def solution():
+    i = 1
+
+    while True:
+        if (
+            sorted(str(i))
+            == sorted(str(2 * i))
+            == sorted(str(3 * i))
+            == sorted(str(4 * i))
+            == sorted(str(5 * i))
+            == sorted(str(6 * i))
+        ):
+            return i
+
+        i += 1
+
+print(solution())
